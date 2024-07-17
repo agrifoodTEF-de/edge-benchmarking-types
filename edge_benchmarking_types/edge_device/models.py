cbcdc078271a2db44394c8b7c99884a1848eb806
Andreas Schliebitz <a.schliebitz@hs-osnabrueck.de>
2024-07-17 14:23:43 +0200

    Setting model as None by default.

--- edge_benchmarking_types/edge_device/models.py
+++ edge_benchmarking_types/edge_device/models.py
@@ -70,7 +70,7 @@ class CpuInfo(BaseModel):
     l1_instruction_cache_size: int
     l2_cache_size: int
     l3_cache_size: int
-    model: Optional[int]
+    model: Optional[int] = Field(default=None)
     flags: List[str]
 
 

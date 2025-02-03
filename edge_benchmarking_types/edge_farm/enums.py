961b2be34eec5f769ea480523f94e795506a551d
Andreas Schliebitz <a.schliebitz@hs-osnabrueck.de>
2025-02-03 11:35:31 +0100

    Dropping InferenceClientConfig in favor of InferenceClient.

--- edge_benchmarking_types/edge_farm/enums.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class InferenceClientType(str, Enum):
-    TRITON_DENSENET = "TritonDenseNetClient"
-    TRITON_YOLO = "TritonYoloClient"
--- edge_benchmarking_types/edge_farm/models.py
+++ edge_benchmarking_types/edge_farm/models.py
@@ -20,12 +20,12 @@ class EdgeDeviceConfig(BaseModel):
     port: Optional[int] = Field(default=None)
 
 
-class InferenceClientConfig(BaseModel):
+class InferenceClient(BaseModel):
     protocol: str = Field(default="http")
     host: str
 
 
-class TritonInferenceClientConfig(InferenceClientConfig):
+class TritonInferenceClient(InferenceClient):
     model_name: Optional[str] = Field(default=None)
     model_version: str = Field(default="1")
     batch_size: int = Field(default=1)
@@ -33,23 +33,18 @@ class TritonInferenceClientConfig(InferenceClientConfig):
     model_config = ConfigDict(protected_namespaces=())
 
 
-class TritonDenseNetClientConfig(TritonInferenceClientConfig):
+class TritonDenseNetClient(TritonInferenceClient):
     num_classes: int = Field(default=0)
     scaling: Optional[str] = Field(default=None)
 
 
-class TritonYoloClientConfig(TritonInferenceClientConfig):
+class TritonYoloClient(TritonInferenceClient):
     num_classes: int = Field(default=0)
     scaling: Optional[str] = Field(default=None)
     confidence_thres: float = Field(default=0.2, ge=0, le=1)
     iou_thres: float = Field(default=0.2, ge=0, le=1)
     input_width: int
     input_height: int
-
-
-class InferenceClient(BaseModel):
-    config: InferenceClientConfig
-    type: InferenceClientType
 
 
 class BenchmarkConfig(BaseModel):

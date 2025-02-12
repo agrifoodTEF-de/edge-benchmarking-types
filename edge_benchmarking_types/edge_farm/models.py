2464774d79cc1ac82f97fa3fb799ebac9c02c427
Andreas Schliebitz <a.schliebitz@hs-osnabrueck.de>
2025-02-12 21:04:05 +0100

    Using Optional instead of |

--- edge_benchmarking_types/edge_farm/models.py
+++ edge_benchmarking_types/edge_farm/models.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field, ConfigDict, field_validator
 
 class DatasetSample(BaseModel):
     filename: str
-    content_type: str | None
+    content_type: Optional[str] = Field(default=None)
     data: Any
 
 

5ebde4dd07a1f8b5cfff5f82ffbed7a779a39fc2
Andreas Schliebitz <a.schliebitz@hs-osnabrueck.de>
2025-09-04 12:10:46 +0200

    If a sensor is offline its RTT has to be None.

--- edge_benchmarking_types/sensors/models.py
+++ edge_benchmarking_types/sensors/models.py
@@ -29,7 +29,7 @@ class SensorConfig(BaseModel):
 class SensorStatus(BaseModel):
     online: bool
     last_seen: datetime
-    rtt: float
+    rtt: Optional[float]
     timestamp: datetime = Field(default_factory=datetime.now)
 
 
